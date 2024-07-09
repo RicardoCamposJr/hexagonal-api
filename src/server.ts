e9038cc8bcc6141ba4e1909ffe8de65646070f67
Ricardo Campos <rickoliver001@hotmail.com>
2024-07-09 09:55:43 -0300

    fix: removing persistenceArray

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import TaskRepositoryDB from './tasks/adapter/persistence/taskRepositoryDB';
 import TaskController from './tasks/http/rest/taskController';
-// import TaskRepositoryArray from './tasks/adapter/persistenceArray/taskRepositoryArray';
 
 const taskRepository = new TaskRepositoryDB()
-// const taskRepository = new TaskRepositoryArray()
 const taskController = new TaskController(taskRepository)
 
 const app = express();
