272e9c38ca50dda48bf3a5640ab7d123eea762f8
Ricardo Campos <rickoliver001@hotmail.com>
2024-10-24 17:01:21 -0300

    wip: implementing project entity table

--- src/tasks/adapter/persistence/db.ts
+++ src/tasks/adapter/persistence/db.ts
@@ -11,23 +11,44 @@ async function setupDatabase() {
 		database: process.env.DATABASE_NAME,
 	});
 
+	// Criando tabela de usuários
 	await connection.query(`CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
-  )`);
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		username VARCHAR(255) NOT NULL,
+		password VARCHAR(255) NOT NULL,
+		email VARCHAR(255) NOT NULL UNIQUE
+	)`);
 
+	// Criando tabela de projetos
+	await connection.query(`CREATE TABLE IF NOT EXISTS projects (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(255) NOT NULL,
+		description TEXT,
+		createdAt VARCHAR(255)
+	)`);
+
+	// Tabela de associação entre projetos e usuários (Muitos-para-Muitos)
+	await connection.query(`CREATE TABLE IF NOT EXISTS project_users (
+		project_id INT,
+		user_id INT,
+		PRIMARY KEY (project_id, user_id),
+		FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	)`);
+
+	// Criando tabela de tarefas
 	await connection.query(`CREATE TABLE IF NOT EXISTS tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    status ENUM('active', 'concluded', 'removed') NOT NULL DEFAULT 'active',
-    priority ENUM('low', 'medium', 'high') NOT NULL,
-    createdAt VARCHAR(255),
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-  )`);
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		title VARCHAR(255) NOT NULL,
+		description TEXT,
+		status ENUM('active', 'concluded', 'removed') NOT NULL DEFAULT 'active',
+		priority ENUM('low', 'medium', 'high') NOT NULL,
+		createdAt VARCHAR(255),
+		project_id INT,
+		user_id INT,
+		FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	)`);
 
 	return connection;
 }
